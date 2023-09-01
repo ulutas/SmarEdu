cb808891b6389827a1ba4dab10c1f724e156ae5d
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-09-01 14:12:02 +0300

    app.js added

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+
+const app = express();
+
+//Connect DB
+mongoose
+  .connect('mongodb://127.0.0.1:27017/smartedu-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected Successfully');
+  });
+
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
+});
