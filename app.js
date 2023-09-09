06ee750edceb3ed0578a6a0c3a2268cbfda0028b
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-09-09 21:46:57 +0300

    authController.js added

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
@@ -11,6 +16,8 @@ mongoose
   .connect('mongodb://127.0.0.1:27017/smartedu-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
   })
   .then(() => {
     console.log('DB Connected Successfully');
@@ -19,14 +26,32 @@ mongoose
 //Template Engine
 app.set('view engine', 'ejs');
 
+//Global Variable
+global.userIN = null;
+
+
 //Middlewares
 app.use(express.static('public'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/smartedu-db' })
+  })
+);
 
 //Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+})
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ controllers/authController.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (user) {
+      bcrypt.compare(password, user.password, (err, same) => {
+        if (same) {
+          //USER SESSION
+          req.session.userID = user._id;
+          res.status(200).redirect('/users/dashboard');
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+};
+exports.logoutUser = async (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+};
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user,
+  });
+};
