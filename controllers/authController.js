df707e00d8e8d112a1c0c53d257fcc4db4005a4c
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-09-16 23:02:20 +0300

    authController.js and courseController.js modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -21,11 +21,9 @@ exports.loginUser = async (req, res) => {
     const user = await User.findOne({ email: email });
     if (user) {
       bcrypt.compare(password, user.password, (err, same) => {
-        if (same) {
           //USER SESSION
           req.session.userID = user._id;
           res.status(200).redirect('/users/dashboard');
-        }
       });
     }
   } catch (error) {
@@ -41,7 +39,7 @@ exports.logoutUser = async (req, res) => {
   });
 };
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate('courses');
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
   res.status(200).render('dashboard', {
