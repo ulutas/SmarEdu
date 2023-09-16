bddfea7a40790bc8c0f1545f707393f18bc4d2a2
Ömer ULUTAŞ <omerulutas@outlook.com>
2023-09-17 01:16:23 +0300

    contact page modified

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -22,14 +22,28 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
+
     const category = await Category.findOne({ slug: categorySlug });
 
     let filter = {};
     if (categorySlug) {
       filter = { category: category._id };
     }
+    if (query) {
+      filter = { name: query };
+    }
+    if (!query && !categorySlug) {
+      filter.name = '';
+      filter.category = null;
+    }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    const courses = await Course.find({
+      $or: [
+        { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+        { category: filter.category },
+      ],
+    }).sort('-createdAt').populate('user');
     const categories = await Category.find();
 
     res.status(200).render('courses', {
@@ -51,10 +65,12 @@ exports.getCourse = async (req, res) => {
     const course = await Course.findOne({ slug: req.params.slug }).populate(
       'user'
     );
+    const categories = await Category.find();
     res.status(200).render('course', {
       course,
       page_name: 'courses',
       user,
+      categories,
     });
   } catch (error) {
     res.status(400).json({
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,3 +1,5 @@
+const nodemailer = require('nodemailer');
+
 exports.getIndexPage = (req, res) => {
   console.log(req.session.userID);
   res.status(200).render('index', {
@@ -9,6 +11,11 @@ exports.getAboutPage = (req, res) => {
     page_name: 'about',
   });
 };
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
 exports.getRegisterPage = (req, res) => {
   res.status(200).render('register', {
     page_name: 'register',
@@ -18,4 +25,50 @@ exports.getLoginPage = (req, res) => {
   res.status(200).render('login', {
     page_name: 'login',
   });
+};
+exports.sendEmail = async (req, res) => {
+  try {
+    const outputMessage = `
+    
+    <h1>Mail Details </h1>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Email: ${req.body.email}</li>
+    </ul>
+    <h1>Message</h1>
+    <p>${req.body.message}</p>
+    `;
+
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'loraine.langosh76@ethereal.email',
+        pass: 'CCuzvsbF6NYazwNyKa',
+      },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <loraine.langosh76@ethereal.email>', // sender address
+      to: 'ulutasuomer@gmail.com', // list of receivers
+      subject: 'Smart EDU Contact Form New Message ✔', // Subject line
+      html: outputMessage, // html body
+    });
+
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+    // req.flash('success', 'We Received your message succesfully');
+
+    res.status(200).redirect('contact');
+  } catch (err) {
+    //req.flash("error", `Something happened! ${err}`);
+    // req.flash('error', `Something happened!`);
+    res.status(200).redirect('contact');
+  }
 };
